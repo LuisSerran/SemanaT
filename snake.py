addadd6c2a2bf9973b9dc9d76c7c510d8466f5e4
LuisFerFlores31 <a01738347@tec.mx>
2024-09-10 11:22:43 -0600

    ele

--- snake.py
+++ snake.py
@@ -61,7 +61,17 @@ def move():
     update()
     ontimer(move, 100)
 
+def move_food():
+    """Move food randomly one step at a time."""
+    directions = [vector(10, 0), vector(-10, 0), vector(0, 10), vector(0, -10)]
+    move_direction = choice(directions)
+    food_move = food + move_direction
 
+    if inside(food_move):  
+        food.move(move_direction)
+
+
+move_food()  
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
