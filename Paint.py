350dc9126e58665563081fb6e5ef107077ea7245
Luis Serrano <luis.zempo15@gmail.com>
2024-09-10 13:13:16 -0600

    hice el circulo

--- Paint.py
+++ Paint.py
@@ -11,6 +11,7 @@ Exercises
 
 from turtle import *
 from freegames import vector
+import turtle 
 
 
 def line(start, end):
@@ -37,16 +38,16 @@ def square(start, end):
 
 def circle(start, end):
     """Draw circle from start to end."""
-    up()
-    goto(start.x, start.y)
-    down()
-    begin_fill()
+    turtle.up()
+    turtle.goto(start.x, start.y)
+    turtle.down()
+    turtle.begin_fill()
 
     #radio
     radius = ((end.x - start.x)**2 + (end.y - start.y)**2)**0.5
     turtle.circle(radius)
 
-    end_fill()
+    turtle.end_fill()
 
 def rectangle(start, end):
     """Draw rectangle from start to end."""
